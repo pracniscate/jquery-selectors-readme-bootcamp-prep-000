3e06881e1c06237502eae3748695898d15153632
Lorena <lorena.giddens@outlook.com>
2018-04-18 05:29:11 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,18 +5,22 @@ function paragraphSelector() {
   return $('p');
 }
 
+// last selector
 function lastImageSelector() {
   return $('img:last');
 }
 
+// ID selector
 function ninjaBabySelector() {
   return $('#baby-ninja');
 }
 
+// class selector
 function divSelector() {
   return $('.pics');
 }
 
+// first-child selector
 function firstListItem() {
   return $('#pic-list li:first-child');
 }
